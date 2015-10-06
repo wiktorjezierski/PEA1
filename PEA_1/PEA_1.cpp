7ef2adb9b0ecc6863732bd41ec295ddcc50ddb9b
Wiktor <wiktor.jezierski@interia.pl>
2015-10-06 08:37:41 +0200

    drugi commit

--- PEA_1/PEA_1.cpp
+++ PEA_1/PEA_1.cpp
@@ -1,11 +1,13 @@
 // PEA_1.cpp : Defines the entry point for the console application.
 //
-
 #include "stdafx.h"
-
+using namespace std;
 
 int main()
 {
+	cout << "witaj!\n";
+	cin.ignore();
+	cin.get();
     return 0;
 }
 
