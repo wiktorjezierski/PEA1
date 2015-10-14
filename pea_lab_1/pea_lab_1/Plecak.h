732a7731f8eccc68adf38f6d947e6028537acd58
Wiktor <wiktor.jezierski@interia.pl>
2015-10-14 11:32:13 +0200

    wyrzucenie wczytywania z konstruktora do funkcji wczytaj_plik_testowy()

--- pea_lab_1/pea_lab_1/Plecak.h
+++ pea_lab_1/pea_lab_1/Plecak.h
@@ -68,80 +68,80 @@ class Plecak
 public:
 	Plecak()
 	{
-		string nazwa_pliku;
-		srand(time(NULL));
-	    clock_t start, koniec;
-		int menu;
+		//string nazwa_pliku;
+		//srand(time(NULL));
+	 //   clock_t start, koniec;
+		//int menu;
 
-		cout << "jesli chcesz wczytac i wyswietlic dane z pliku nacisnij 1\njesli chcesz wygenerowac losowa instancje nacisnij 2\n";
-		cin >> menu;
-		
-		if (menu == 1)
-		{
-			cout << "\npodaj nazwe pliku z instancja testowa\n";
-			cin >> nazwa_pliku;
-		}
-		if (menu == 2)
-		{
-			ofstream plik_generuj;
-			int pom_pojemnosc;
-			int pom_ilosc_elementow;
-			plik_generuj.open("generowany.txt");
+		//cout << "jesli chcesz wczytac i wyswietlic dane z pliku nacisnij 1\njesli chcesz wygenerowac losowa instancje nacisnij 2\n";
+		//cin >> menu;
+		//
+		//if (menu == 1)
+		//{
+		//	cout << "\npodaj nazwe pliku z instancja testowa\n";
+		//	cin >> nazwa_pliku;
+		//}
+		//if (menu == 2)
+		//{
+		//	ofstream plik_generuj;
+		//	int pom_pojemnosc;
+		//	int pom_ilosc_elementow;
+		//	plik_generuj.open("generowany.txt");
 
-			cout << "Podaj pojemnosc plecaka\n";
-			cin >> pom_pojemnosc;
-			plik_generuj << "\n" << pom_pojemnosc;	
+		//	cout << "Podaj pojemnosc plecaka\n";
+		//	cin >> pom_pojemnosc;
+		//	plik_generuj << "\n" << pom_pojemnosc;	
 
-			cout << "Podaj ilosc elementow\n";
-			cin >> pom_ilosc_elementow;
-			plik_generuj << "\n" << pom_ilosc_elementow;
+		//	cout << "Podaj ilosc elementow\n";
+		//	cin >> pom_ilosc_elementow;
+		//	plik_generuj << "\n" << pom_ilosc_elementow;
 
-			for (int i = 0; i < pom_ilosc_elementow; i++)
-			{
-				
-					plik_generuj << "\n" << rand() %(int) (pom_pojemnosc*0.5) + 1 << "\t" << rand() % pom_pojemnosc + 1;
-			}
-			//nazwa_pliku = "generowany.txt";
-			nazwa_pliku = "generowany";
-			plik_generuj.close();			
-			
-		}
+		//	for (int i = 0; i < pom_ilosc_elementow; i++)
+		//	{
+		//		
+		//			plik_generuj << "\n" << rand() %(int) (pom_pojemnosc*0.5) + 1 << "\t" << rand() % pom_pojemnosc + 1;
+		//	}
+		//	//nazwa_pliku = "generowany.txt";
+		//	nazwa_pliku = "generowany";
+		//	plik_generuj.close();			
+		//	
+		//}
 
-			if (menu == 1 || menu == 2)
-			{
-			if (wczytaj_plik_testowy(nazwa_pliku+".txt"))
-			{
-				//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
-				pojemnosc = wektor_liczb.front();
-				//usuniecie pierwszego elementu wektora
-				wektor_liczb.erase(wektor_liczb.begin());
-				//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
-				ilosc_elementow = wektor_liczb.front();
-				//usuniecie pierwszego elementu wektora
-				wektor_liczb.erase(wektor_liczb.begin());
-				cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
-				if (wektor_liczb.size() == 2 * ilosc_elementow)
-				{
-					//stworzenie wektora zawierajacego elementy plecaka
-					for (int i = 0; i < wektor_liczb.size(); i = i + 2)
-					{
-						wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));						
-					}
+		//	if (menu == 1 || menu == 2)
+		//	{
+		//	if (wczytaj_plik_testowy(nazwa_pliku+".txt"))
+		//	{
+		//		//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
+		//		pojemnosc = wektor_liczb.front();
+		//		//usuniecie pierwszego elementu wektora
+		//		wektor_liczb.erase(wektor_liczb.begin());
+		//		//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
+		//		ilosc_elementow = wektor_liczb.front();
+		//		//usuniecie pierwszego elementu wektora
+		//		wektor_liczb.erase(wektor_liczb.begin());
+		//		cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
+		//		if (wektor_liczb.size() == 2 * ilosc_elementow)
+		//		{
+		//			//stworzenie wektora zawierajacego elementy plecaka
+		//			for (int i = 0; i < wektor_liczb.size(); i = i + 2)
+		//			{
+		//				wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));						
+		//			}
 
-					for each (Przedmiot p in wektor_przedmiotow)
-					{
-						cout << "rozmiar " << p.rozmiar << " wartosc " << p.wartosc << endl;
-					}
-				}
-			}
-			else
-			{
-				cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
-			}	
-							
-				
-					//sortowanie wektora zawierajacego przedmioty funkcja sort z STL, 
-					sort(wektor_przedmiotow.begin(), wektor_przedmiotow.end(), porownaj);
+		//			for each (Przedmiot p in wektor_przedmiotow)
+		//			{
+		//				cout << "rozmiar " << p.rozmiar << " wartosc " << p.wartosc << endl;
+		//			}
+		//		}
+		//	}
+		//	else
+		//	{
+		//		cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
+		//	}	
+		//					
+		//		
+		//			//sortowanie wektora zawierajacego przedmioty funkcja sort z STL, 
+		//			sort(wektor_przedmiotow.begin(), wektor_przedmiotow.end(), porownaj);
 					//wywo³anie algorytmu podzia³u i ograniczen
 					//branch_and_bound();
 					////dorzucic tu wywolanie drugiego algorytmu
@@ -167,7 +167,7 @@ public:
 					//	wyswietl_wybrane_elementy();
 					//}		
 						
-		}		
+		//}		
 		
 		
 	}
@@ -401,6 +401,85 @@ public:
 		if (wybor == 1)
 		{
 			wyswietl_wybrane_elementy();
+		}
+	}
+
+	void wczzytaj()
+	{
+		string nazwa_pliku;
+		srand(time(NULL));
+		clock_t start, koniec;
+		int menu;
+
+		cout << "jesli chcesz wczytac i wyswietlic dane z pliku nacisnij 1\njesli chcesz wygenerowac losowa instancje nacisnij 2\n";
+		cin >> menu;
+
+		if (menu == 1)
+		{
+			cout << "\npodaj nazwe pliku z instancja testowa\n";
+			cin >> nazwa_pliku;
+		}
+		if (menu == 2)
+		{
+			ofstream plik_generuj;
+			int pom_pojemnosc;
+			int pom_ilosc_elementow;
+			plik_generuj.open("generowany.txt");
+
+			cout << "Podaj pojemnosc plecaka\n";
+			cin >> pom_pojemnosc;
+			plik_generuj << "\n" << pom_pojemnosc;
+
+			cout << "Podaj ilosc elementow\n";
+			cin >> pom_ilosc_elementow;
+			plik_generuj << "\n" << pom_ilosc_elementow;
+
+			for (int i = 0; i < pom_ilosc_elementow; i++)
+			{
+
+				plik_generuj << "\n" << rand() % (int)(pom_pojemnosc*0.5) + 1 << "\t" << rand() % pom_pojemnosc + 1;
+			}
+			//nazwa_pliku = "generowany.txt";
+			nazwa_pliku = "generowany";
+			plik_generuj.close();
+
+		}
+
+		if (menu == 1 || menu == 2)
+		{
+			if (wczytaj_plik_testowy(nazwa_pliku + ".txt"))
+			{
+				//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
+				pojemnosc = wektor_liczb.front();
+				//usuniecie pierwszego elementu wektora
+				wektor_liczb.erase(wektor_liczb.begin());
+				//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
+				ilosc_elementow = wektor_liczb.front();
+				//usuniecie pierwszego elementu wektora
+				wektor_liczb.erase(wektor_liczb.begin());
+				cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
+				if (wektor_liczb.size() == 2 * ilosc_elementow)
+				{
+					//stworzenie wektora zawierajacego elementy plecaka
+					for (int i = 0; i < wektor_liczb.size(); i = i + 2)
+					{
+						wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));
+					}
+
+					for each (Przedmiot p in wektor_przedmiotow)
+					{
+						cout << "rozmiar " << p.rozmiar << " wartosc " << p.wartosc << endl;
+					}
+				}
+			}
+			else
+			{
+				cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
+			}
+
+
+			//sortowanie wektora zawierajacego przedmioty funkcja sort z STL, 
+			sort(wektor_przedmiotow.begin(), wektor_przedmiotow.end(), porownaj);
 		}
 	}
 
