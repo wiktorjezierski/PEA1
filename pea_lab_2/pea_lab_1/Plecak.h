90c6e5795e4395476593c3169350a5971505e0f5
Wiktor Jezierski <wiktor.jezierski@interia.pl>
2015-11-11 16:09:04 +0100

    poprawione PEA_lab2

--- pea_lab_2/pea_lab_1/Plecak.h
+++ pea_lab_2/pea_lab_1/Plecak.h
@@ -3,8 +3,7 @@
 #include <algorithm>
 #include <stdio.h>
 #include <time.h>
-#include <ctime>
-#include <chrono>
+//#include <ctime>
 
 using namespace std;
 
@@ -20,7 +19,6 @@ struct Przedmiot
 		this->wartosc = parametr_wartosc;
 		//wartosc i rozmiar sa rzutowane na double po to aby wynik dzielenia posiada³ czesc ulamkowa
 		this->wartosc_do_rozmiaru = (double)(wartosc) / (double)(rozmiar);
-
 	}
 };
 class Plecak
@@ -30,126 +28,125 @@ class Plecak
 	int ilosc_elementow;
 	vector<Przedmiot> wektor_przedmiotow;
 	vector<int>wektor_liczb;
-	int **tablica_wartosci=NULL;
+	int **tablica_wartosci = NULL;
 
 public:
 	Plecak()
 	{
-		string nazwa_pliku;	    
-		int menu;
-
-		cout << "jesli chcesz wczytac dane z pliku nacisnij 1\njesli chcesz wygenerowac losowa instancje nacisnij 2\n";
-		cin >> menu;
-		
-		if (menu == 1)
-		{
-			cout << "\npodaj nazwe pliku z instancja testowa\n";
-			cin >> nazwa_pliku;
-		}
-		if (menu == 2)
-		{
-			ofstream plik_generuj;
-			int pom_pojemnosc;
-			int pom_ilosc_elementow;
-			plik_generuj.open("generowany.txt");
-			cout << "Podaj pojemnosc plecaka\n";
-			cin >> pom_pojemnosc;
-			plik_generuj << "\n" << pom_pojemnosc;
-			cout << "Podaj ilosc elementow\n";
-			cin >> pom_ilosc_elementow;
-			plik_generuj << "\n" << pom_ilosc_elementow;
-
-			for (int i = 0; i < pom_ilosc_elementow; i++)
-			{
-				
-					plik_generuj << "\n" << rand() %(int) (pom_pojemnosc*0.5) + 1 << "\t" << rand() % pom_pojemnosc + 1;
-			}
-			nazwa_pliku = "generowany.txt";
-			plik_generuj.close();			
-			
-		}
-
-			if (menu == 1 || menu == 2)
-			{
-			if (wczytaj_plik_testowy(nazwa_pliku))
-			{
-				//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
-				pojemnosc = wektor_liczb.front();
-				//usuniecie pierwszego elementu wektora
-				wektor_liczb.erase(wektor_liczb.begin());
-				//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
-				ilosc_elementow = wektor_liczb.front();
-				//usuniecie pierwszego elementu wektora
-				wektor_liczb.erase(wektor_liczb.begin());
-				cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
-				if (wektor_liczb.size() == 2 * ilosc_elementow)
-				{
-					//stworzenie wektora zawierajacego elementy plecaka
-					for (int i = 0; i < wektor_liczb.size(); i = i + 2)
-					{
-						wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));
-					}
-
-					tablica_wartosci = new  int*[ilosc_elementow+1];
-					for (int i = 0; i < ilosc_elementow+1; i++)
-					{
-						tablica_wartosci[i] = new int[pojemnosc+1];
-						
-					}
-					programowanie_dynamiczne();
-				}
-			}
-			else
-			{
-				cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
-			}
-												
-		}		
-		
-		
+		//string nazwa_pliku;	    
+		//int menu;
+		//
+		//cout << "jesli chcesz wczytac dane z pliku nacisnij 1\njesli chcesz wygenerowac losowa instancje nacisnij 2\n";
+		//cin >> menu;
+		//
+		//if (menu == 1)
+		//{
+		//	cout << "\npodaj nazwe pliku z instancja testowa\n";
+		//	cin >> nazwa_pliku;
+		//}
+		//if (menu == 2)
+		//{
+		//	ofstream plik_generuj;
+		//	int pom_pojemnosc;
+		//	int pom_ilosc_elementow;
+		//	plik_generuj.open("generowany.txt");
+		//	cout << "Podaj pojemnosc plecaka\n";
+		//	cin >> pom_pojemnosc;
+		//	plik_generuj << "\n" << pom_pojemnosc;
+		//	cout << "Podaj ilosc elementow\n";
+		//	cin >> pom_ilosc_elementow;
+		//	plik_generuj << "\n" << pom_ilosc_elementow;
+		//
+		//	for (int i = 0; i < pom_ilosc_elementow; i++)
+		//	{
+		//		
+		//			plik_generuj << "\n" << rand() %(int) (pom_pojemnosc*0.5) + 1 << "\t" << rand() % pom_pojemnosc + 1;
+		//	}
+		//	nazwa_pliku = "generowany.txt";
+		//	plik_generuj.close();			
+		//	
+		//}
+		//
+		//	if (menu == 1 || menu == 2)
+		//	{
+		//	if (wczytaj_plik_testowy(nazwa_pliku))
+		//	{
+		//		//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
+		//		pojemnosc = wektor_liczb.front();
+		//		//usuniecie pierwszego elementu wektora
+		//		wektor_liczb.erase(wektor_liczb.begin());
+		//		//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
+		//		ilosc_elementow = wektor_liczb.front();
+		//		//usuniecie pierwszego elementu wektora
+		//		wektor_liczb.erase(wektor_liczb.begin());
+		//		cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
+		//		if (wektor_liczb.size() == 2 * ilosc_elementow)
+		//		{
+		//			//stworzenie wektora zawierajacego elementy plecaka
+		//			for (int i = 0; i < wektor_liczb.size(); i = i + 2)
+		//			{
+		//				wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));
+		//			}
+		//
+		//			tablica_wartosci = new  int*[ilosc_elementow+1];
+		//			for (int i = 0; i < ilosc_elementow+1; i++)
+		//			{
+		//				tablica_wartosci[i] = new int[pojemnosc+1];
+		//				
+		//			}
+		//			programowanie_dynamiczne();
+		//		}
+		//	}
+		//	else
+		//	{
+		//		cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
+		//	}
+		//										
+		//}			
 	}
-	Plecak(string nazwa_pliku)
-	{
-		if (wczytaj_plik_testowy(nazwa_pliku))
-		{
-			//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
-			pojemnosc = wektor_liczb.front();
-			//usuniecie pierwszego elementu wektora
-			wektor_liczb.erase(wektor_liczb.begin());
-			//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
-			ilosc_elementow = wektor_liczb.front();
-			//usuniecie pierwszego elementu wektora
-			wektor_liczb.erase(wektor_liczb.begin());
-			cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
-			if (wektor_liczb.size() == 2 * ilosc_elementow)
-			{
-				//stworzenie wektora zawierajacego elementy plecaka
-				for (int i = 0; i < wektor_liczb.size(); i = i + 2)
-				{
-					wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));
-				}
 
-				tablica_wartosci = new  int*[ilosc_elementow + 1];
-				for (int i = 0; i < ilosc_elementow + 1; i++)
-				{
-					tablica_wartosci[i] = new int[pojemnosc + 1];
+	//Plecak(string nazwa_pliku)
+	//{
+	//	if (wczytaj_plik_testowy(nazwa_pliku))
+	//	{
+	//		//pobranie pojemnosci plecaka, oraz usuniecie tej danej z wektora
+	//		pojemnosc = wektor_liczb.front();
+	//		//usuniecie pierwszego elementu wektora
+	//		wektor_liczb.erase(wektor_liczb.begin());
+	//		//pobranie ilosci elementow, oraz usuniecie tej danej z wektora
+	//		ilosc_elementow = wektor_liczb.front();
+	//		//usuniecie pierwszego elementu wektora
+	//		wektor_liczb.erase(wektor_liczb.begin());
+	//		cout << "Problem plecakowy\nzlodziej ma do dyspozycji\t" << ilosc_elementow << " elementow\n" << "pojemnosc plecaka wynosi\t" << pojemnosc << "\n";
+	//		if (wektor_liczb.size() == 2 * ilosc_elementow)
+	//		{
+	//			//stworzenie wektora zawierajacego elementy plecaka
+	//			for (int i = 0; i < wektor_liczb.size(); i = i + 2)
+	//			{
+	//				wektor_przedmiotow.push_back(Przedmiot(wektor_liczb[i], wektor_liczb[i + 1]));
+	//			}
+	//
+	//			tablica_wartosci = new  int*[ilosc_elementow + 1];
+	//			for (int i = 0; i < ilosc_elementow + 1; i++)
+	//			{
+	//				tablica_wartosci[i] = new int[pojemnosc + 1];
+	//
+	//			}
+	//		programowanie_dynamiczne();
+	//		}
+	//	}
+	//	else
+	//	{
+	//		cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
+	//	}
+	//}
 
-				}
-			programowanie_dynamiczne();
-			}
-		}
-		else
-		{
-			cout << "Niepoprawna ilosc wczytanych wartosci\nalgorytm nie wykona sie :(\n";
-		}
-	}
 	~Plecak()
 	{
-		wektor_przedmiotow.erase(wektor_przedmiotow.begin(), wektor_przedmiotow.end());
-		wektor_liczb.erase(wektor_liczb.begin(), wektor_liczb.end());
+		wektor_przedmiotow.clear();
+		wektor_liczb.clear();
 		if (tablica_wartosci!=NULL)
 		{
-
 			for (int i = 0; i <= ilosc_elementow; i++)
 			{
 				delete[] tablica_wartosci[i];
@@ -158,6 +155,7 @@ public:
 			delete[] tablica_wartosci;
 		}
 	}
+
 	bool wczytaj_plik_testowy(string nazwa_pliku)
 	{
 		vector<string> wektor_odczytu;
@@ -193,18 +191,161 @@ public:
 			cout << "Niepoprawny odczyt pliku\n";
 			return false;
 		}
+	}
+
+	void zarzadzaj()
+	{
+		int wybor = 666;
+		int wybor2;
+		bool czy_wczytano = false;
+		string nazwaPliku;
+
+		cout << "Podaj rozmiar plecaka: ";
+		cin >> pojemnosc;
+		system("cls");
+
+		while (wybor != 0) {
+			wybor2 = 666;
+			cout << "\tMENU\n";
+			cout << "Wybierz opcje:\n";
+			cout << "-----------------------------\n";
+			cout << "1. Wczytaj z pliku\n";
+			cout << "2. Wygeneruj losowo\n";
+			cout << "3. Zmien rozmiar plecaka\n";
+			cout << "4. Algorytm programowania dynamicznego\n";
+			cout << "0. EXIT\n";
+			cout << "-----------------------------\n\n";
+			cin >> wybor;
+			switch (wybor)
+			{
+			case 1:
+				cout << "Podaj nazwe pliku:\n";
+				cin >> nazwaPliku;
+				czy_wczytano = wczytaj_z_pliku(nazwaPliku);
+				wyswietl_po_wczytaniu();
+				cout << "\n";
+				break;
+			case 2:
+				cout << "Wygeneruj losowo()\nIle elementow wygenerowac?\n> ";
+				int ile;
+				cin >> ile;
+				czy_wczytano = losowo(ile);
+				cout << "\n";
+				break;
+			case 3:
+				cout << "podaj nowy rozmiar plecaka: ";
+				cin >> pojemnosc;
+				cout << "\n";
+				break;
+			case 4:
+				cout << "\nUruchomiono algorytm programowania dynamicznego\n";
+				if (czy_wczytano)
+				{
+					programowanie_dynamiczne();
+				}
+				else
+				{
+					cout << endl << "Nie wczytano wartosci\n";
+				}
+			default:
+				break;
+			}
+		}
+	}
+
+	bool wczytaj_z_pliku(string nazwa_pliku)
+	{
+		fstream plik;
+		string tekst1;
+		stringstream ss;
+
+		plik.open(nazwa_pliku + ".txt", ios::in);
+
+		if (plik.good())
+		{
+			wektor_przedmiotow.clear();
+			stringstream konwersja;
+			getline(plik, tekst1, '\n');
+			konwersja.clear();
+			konwersja << tekst1;
+			konwersja >> ilosc_elementow;
+
+			for (int i = 0; i < ilosc_elementow; i++)
+			{
+				int temp1, temp2;
+
+				getline(plik, tekst1, ' ');			//wczytanie z pliku tekstu do znaku '/'
+				konwersja.clear();
+				konwersja << tekst1;
+				konwersja >> temp1;						//przypisanie zmiennej w obiekcie wartosci				
+
+				getline(plik, tekst1, '\n');			//wczytanie z pliku tekstu do znaku '/'
+				konwersja.clear();
+				konwersja << tekst1;
+				konwersja >> temp2;						//przypisanie zmiennej w obiekcie wartosci			
+
+				Przedmiot item(temp1, temp2);
+				wektor_przedmiotow.push_back(item);
+			}
+			plik.close();
+
+			/*tablica_wartosci = new  int*[ilosc_elementow + 1];
+			for (int i = 0; i < ilosc_elementow + 1; i++)
+			{
+				tablica_wartosci[i] = new int[pojemnosc + 1];
+			}*/
+
+			return true;
+		}
+		else
+		{
+			cout << "blednda nazwa pliku\n";
+			return false;
+		}
 
 	}
+
+	void wyswietl_po_wczytaniu()
+	{
+		for each (Przedmiot item in wektor_przedmiotow)
+		{
+			cout << "rozmiar: " << item.rozmiar << " wartosc: " << item.wartosc << endl;
+		}
+
+		cout << endl;
+	}
+
+	bool losowo(int ilosc)
+	{
+		srand(time(NULL));
+		wektor_przedmiotow.clear();
+		for (int i = 0; i < ilosc; i++)
+		{
+			Przedmiot item(rand() % ilosc, rand() % ilosc);
+			wektor_przedmiotow.push_back(item);
+		}
+
+		/*tablica_wartosci = new  int*[ilosc_elementow + 1];
+		for (int i = 0; i < ilosc_elementow + 1; i++)
+		{
+			tablica_wartosci[i] = new int[pojemnosc + 1];
+		}*/
+		return true;
+	}
+
 	void  programowanie_dynamiczne()
 	{
-		//srand(time(NULL));
-		//clock_t start, koniec;
-		//start = clock();
-		auto begin = std::chrono::high_resolution_clock::now();
+		tablica_wartosci = new  int*[ilosc_elementow + 1];
+		for (int i = 0; i < ilosc_elementow + 1; i++)
+		{
+			tablica_wartosci[i] = new int[pojemnosc + 1];
+		}
+
 		for (int i = 0; i <= ilosc_elementow;i++)
 		{
 			tablica_wartosci[i][0] = 0;
 		}
+
 		for (int i = 0; i <= pojemnosc; i++)
 		{
 			tablica_wartosci[0][i] = 0;
@@ -224,9 +365,6 @@ public:
 				}
 			}
 		}
-		//koniec = clock();
-		auto end = chrono::high_resolution_clock::now();
-		cout << "\n"<<chrono::duration_cast<chrono::milliseconds>(end - begin).count() << "ms" << endl;
 		bool log = true;
 		int wiersz = ilosc_elementow;
 		int kolumna = pojemnosc;
@@ -250,11 +388,6 @@ public:
 
 		} while (log);
 
-		cout << "\n\nnajlepsze rozwiazanie\t" << tablica_wartosci[ilosc_elementow][pojemnosc];
-		
-		
-	}	
-	
-
-	
+		cout << "\n\nnajlepsze rozwiazanie\t" << tablica_wartosci[ilosc_elementow][pojemnosc] << endl;
+	}		
 };
--- pea_lab_2/pea_lab_1/pea_lab_1.cpp
+++ pea_lab_2/pea_lab_1/pea_lab_1.cpp
@@ -1,32 +1,22 @@
 // pea_lab_1.cpp : Defines the entry point for the console application.
-//autor Kacper Przezak 200616
+
 
 #include "stdafx.h"
 #include "Plecak.h"
-#include  <thread>
-#include  <chrono>
 
 using namespace std;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	
 	cout << "Program wykonuje algorytm programowania dynamicznego dla decyzyjnego  problemu plecakowego\n\n";
 
-	
-
 	Plecak*plecak= new Plecak();
-	plecak->~Plecak();
 	
+	plecak->zarzadzaj();
+	delete plecak;
 	
-
-
-
-	
-
-	
-
-	system("pause");
+	cin.ignore();
+	cin.get();
 	return 0;
 }
 
